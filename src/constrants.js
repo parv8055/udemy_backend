9549712810beb72f627b8a0f4f35d36b0dd77cd0
Parv Choudhary <104005053+parv8055@users.noreply.github.com>
2023-12-08 13:49:58 +0530

    new

---
+++ src/constrants.js
@@ -0,0 +1,1 @@
+exports.db_name = "natures"
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,9 @@
 const {connect} = require("mongoose")
+const { db_name } = require("../constrants")
 
 const connectdb=async()=>{
     try {
-        const connectionInstance = await connect(process.env.MONGO_URL)
+        const connectionInstance = await connect(`${process.env.MONGO_URL}/${db_name}`)
         console.log(" 🤖 🤖 Connected to Mongodb !! DB host :" + connectionInstance?.connection?.host)
     } catch (err) {
         console.error("Mongodb connection falied !! ERROR :" + err)
