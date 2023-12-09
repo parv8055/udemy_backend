1af20b0aedcd486df51736b3c4c0adca3155ffbb
Parv Choudhary <104005053+parv8055@users.noreply.github.com>
2023-12-09 15:04:40 +0530

    error handler

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
